781fff2909e066b9c9242d620e4079045c5bf00f
Aman Nurgozhiyev <nurgozhiyevaman@gmail.com>
2024-10-30 21:37:30 +0500

    Create Todo.java

---
+++ Todo.java
@@ -0,0 +1,42 @@
+package MVC;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private boolean completed;
+
+    public Todo() {}
+
+    public Todo(String title) {
+        this.title = title;
+        this.completed = false;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
